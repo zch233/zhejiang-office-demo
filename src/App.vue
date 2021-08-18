adb4dcf98f9de2167aeed8e2581ef022734f00d8
zhangchao <529743595@qq.com>
2021-08-18 23:11:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>1111</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style lang="scss">
+* {padding: 0;margin: 0;}
+#app {
+}
+</style>
