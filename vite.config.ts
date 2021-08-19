095651c09809b6a2ebcd49cf78454810f950b5f6
zhangchao <529743595@qq.com>
2021-08-19 22:13:31 +0800

    feat: update base config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import * as path from 'path';
 const pathResolve = (pathStr: string) =>  path.resolve(__dirname, pathStr)
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueJsx(),
